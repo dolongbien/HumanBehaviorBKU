a78c5a9978d2394de1897e3ef2f7cad43a7584f8
ad0min <boss1905@outlook.com>
2019-05-11 00:44:41 +0700

    fix not found static in localhost :3

--- WebApp/locallibrary/settings.py
+++ WebApp/locallibrary/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
